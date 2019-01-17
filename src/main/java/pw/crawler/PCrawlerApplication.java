93dfe166cc07415ddfe4a85441e65659dd83c27a
pishuiwei <823598326@qq.cpm>
2019-01-17 23:58:02 +0800

    crawler

---
+++ src/main/java/pw/crawler/PCrawlerApplication.java
@@ -0,0 +1,14 @@
+package pw.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PCrawlerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pw/crawler/PCrawlerApplicationTests.java
@@ -0,0 +1,17 @@
+package pw.crawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
